1992580646a3f1a0923933901385c020a5d90d08
ashishgoyal1234 <ashishgoyal123@gmail.com>
2025-11-23 16:27:18 +0530

    Commited initial version of Menu Driven Number Game app

---
+++ ConsoleAppMenuDrivenNumberGame/ConsoleAppMenuDrivenNumberGame/Game.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMenuDrivenNumberGame
+{
+    internal class Game
+    {
+        private int bestscore = 0;
+        private readonly Random random = new Random();
+
+        public void Start()
+        {
+            int target = random.Next(1, 101);
+            int guessCount = 0;
+
+            while (true)
+            {
+                int guess = PromptForGuess();
+                guessCount++;
+
+                if (guess < target)
+                {
+                    Console.WriteLine("Low");
+                }
+                else if (guess > target)
+                {
+                    Console.WriteLine("High");
+                }
+                else
+                {
+                    Console.WriteLine($"Win - You guessed in {guessCount} tries");
+                    UpdateBestScore(guessCount);
+
+                }
+                //return;
+            }
+        }
+
+        private int PromptForGuess()
+        {
+            while (true)
+            {
+                Console.WriteLine("Input your guess from 1 to 100: ");
+                string input = Console.ReadLine() ?? "";
+                //if (int.Parse(input) >= 1 && (int.Parse(input) >= 100))
+                //{
+                //    return (int.Parse(input));
+                //}
+                if (int.TryParse(input, out int guess) && guess >= 1 && guess <= 100)
+                { return guess; }
+                Console.WriteLine("Invalid input. Please enter an integer between 1 and 100.");
+            }
+        }
+
+        private void UpdateBestScore(int currentScore)
+        {
+            //int bestscore = _bestscore;
+            if (bestscore == 0 || currentScore < bestscore)
+            {
+                bestscore = currentScore;
+                Console.WriteLine("You got a new best score");
+            }
+        }
+
+        public void ShowBestScore()
+        {
+            if (bestscore == 0)
+                Console.WriteLine("No best score yet.");
+            else
+                Console.WriteLine($"Best score is {bestscore} attempts");
+        }
+
+        public void ResetBestScore()
+        {
+            bestscore = 0;
+            Console.WriteLine("Best scoer has been reset.");
+        }
+    }
+}
---
+++ ConsoleAppMenuDrivenNumberGame/ConsoleAppMenuDrivenNumberGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleAppMenuDrivenNumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            while (true)
+            {
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("----------");
+                Console.WriteLine("1: Start new game");
+                Console.WriteLine("2: Show best score");
+                Console.WriteLine("3: Reset best score");
+                Console.WriteLine("4: Exit");
+                Console.WriteLine("Chose an option and enter the option number");
+
+                string input = Console.ReadLine() ?? "";
+                string[] options = { "1", "2", "3", "4" };
+                bool valid = false;
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (input == options[i])
+                    {
+                        valid = true;
+                        if (i == 0)
+                            game.Start();
+
+                        else if (i == 1)
+                            game.ShowBestScore();
+
+                        else if (i == 2)
+                            game.ResetBestScore();
+                        else if (i == 3)
+                        {
+                            Console.WriteLine("Good bye");
+                            return;
+                        }
+                    }
+                    Console.WriteLine("Invalid selection, please choose 1-4");
+                }
+            }
+        }
+    }
+}
